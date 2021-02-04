6f398123bc4ac8ff25356f97b977e4c495e2d855
Zhu Meng <z_meng@fanshaweonline.ca>
2021-02-03 22:45:19 -0500

    allow user login to welcome page

---
+++ admin/admin_login.php
@@ -0,0 +1,47 @@
+<?php
+require_once '../load.php';
+
+$ip = $_SERVER['REMOTE_ADDR'];//"REMOTE_ADDR" => The IP address from which the user is viewing the current page.
+//we want get the value $ip, and add it in login() as the third parameter
+
+//gain username and password, empty check
+if(isset($_POST['submit'])){
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']); //The trim() function removes whitespace and other predefined characters from both sides of a string.
+    
+    if(!empty($username) && !empty($password)){//if username and password both not empty
+        $result = login($username, $password, $ip);//allow login, login function in login.php
+        $message = $result;
+    }else{
+        $message = 'Plesase fill out the request field';
+    }
+}
+
+
+//if user already log in, redirect user to welcome.php, dont allow login in user access admin_login.php again
+if(isset($_SESSION['user_id'])){
+    redirect_to('welcome.php');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to the admin panel</title>
+</head>
+<body>
+    <?php echo !empty($message)?$message:'';?> <!--if $message isnt empty, print $message info-->
+
+    <form action="admin_login.php" method="post">
+       <label for="username">Username:</label>
+       <input id="username" type="text" name="username" value="">
+       <br><br>
+       <label for="password">Password:</label>
+       <input id="password" type="text" name="password" value="">
+       <br><br>
+       <button type="submit" name="submit">Login</button>
+    </form>
+</body>
+</html>
---
+++ admin/welcome.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../load.php';
+confirm_logged_in();//only login in user can see the index.php page
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+</head>
+<body>
+    <h2>Hello <?php echo $_SESSION['user_name'];?>! Welcome To Our Page. </h2>
+
+    <a href="admin_logout.php">Sign Out</a>
+</body>
+</html>
